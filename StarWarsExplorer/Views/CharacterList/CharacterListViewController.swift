c4fed7c6859bc99dbd70538032ae38ba75be7f0d
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-16 00:48:26 +0300

    add: temp UI for character List

--- StarWarsExplorer/Views/CharacterList/CharacterListViewController.swift
+++ StarWarsExplorer/Views/CharacterList/CharacterListViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class CharacterListViewController: UIViewController {
+class CharacterListViewController: UIViewController, UITableViewDataSource {
+    
+    let viewModel = CharacterListViewModel()
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        
+        view.backgroundColor = .white
+        title = "Star Wars Characters"
+        
+        setupTableView()
+        
+        viewModel.fetchCharacters()
+        
+        viewModel.onDataFetched = {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    private func setupTableView(){
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let character = viewModel.character(at: indexPath.row)
+        cell.textLabel?.text = character.name
+        return cell
     }
 
 
