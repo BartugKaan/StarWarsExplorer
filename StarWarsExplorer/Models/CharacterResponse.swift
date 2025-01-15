e47b0bc681dd222ee66eb0a0e7bc0c51e8b7a358
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-16 00:48:41 +0300

    add: Character Response model

---
+++ StarWarsExplorer/Models/CharacterResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterResponse.swift
+//  StarWarsExplorer
+//
+//  Created by Bartuğ Kaan Çelebi on 16.01.2025.
+//
+
+import Foundation
+
+struct CharacterResponse: Decodable{
+    let results: [Character]
+}
