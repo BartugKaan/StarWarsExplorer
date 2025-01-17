c414e1867f81450847cc181ca367baf799efa7b5
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-17 20:25:54 +0300

    refactor: UITable to UICollectionView

--- StarWarsExplorer/Views/CharacterList/CharacterListViewController.swift
+++ StarWarsExplorer/Views/CharacterList/CharacterListViewController.swift
@@ -7,46 +7,60 @@
 
 import UIKit
 
-class CharacterListViewController: UIViewController, UITableViewDataSource {
+class CharacterListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
     
     let viewModel = CharacterListViewModel()
-    var tableView: UITableView!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        title = "Star Wars Characters"
+        title = "Characters"
         
-        setupTableView()
+        setupCollectionView()
+        
         
         viewModel.fetchCharacters()
         
         viewModel.onDataFetched = {
             DispatchQueue.main.async {
-                self.tableView.reloadData()
+                self.collectionView.reloadData()
             }
         }
     }
     
-    private func setupTableView(){
-        tableView = UITableView(frame: view.bounds, style: .plain)
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        view.addSubview(tableView)
+    private func setupCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: (view.frame.width / 2) - 15, height: 250)
+        layout.minimumLineSpacing = 15
+        layout.minimumInteritemSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(CharacterCell.self, forCellWithReuseIdentifier: CharacterCell.identifier)
+        view.addSubview(collectionView)
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numberOfRowsInSection()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterCell.identifier, for: indexPath) as? CharacterCell else{
+            return UICollectionViewCell()
+        }
         let character = viewModel.character(at: indexPath.row)
-        cell.textLabel?.text = character.name
+        cell.configure(with: character)
         return cell
     }
+    
 
 
 }
