ee338d2a094e48b48ea5b1da5b495d097a8ac876
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-18 01:57:17 +0300

    add: Starship Cell

---
+++ StarWarsExplorer/Views/StarshipList/Cell/StarshipCell.swift
@@ -0,0 +1,58 @@
+//
+//  StarshipCell.swift
+//  StarWarsExplorer
+//
+//  Created by Bartuğ Kaan Çelebi on 17.01.2025.
+//
+
+import UIKit
+import Kingfisher
+
+class StarshipCell: UICollectionViewCell {
+    static let identifier = "StarshipCell"
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 20
+        return iv
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        contentView.addSubview(nameLabel)
+        contentView.layer.cornerRadius = 10
+        contentView.layer.masksToBounds = true
+        contentView.backgroundColor = .darkGray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = CGRect(x: 0, y: 10, width: contentView.frame.width, height: contentView.frame.width)
+        nameLabel.frame = CGRect(x: 5, y: contentView.frame.width + 5, width: contentView.frame.width - 10, height: 40)
+    }
+    
+    func configure(with starship: Starship){
+        nameLabel.text = starship.name
+        
+        if let imageUrl = starship.imageUrl, let url = URL(string: imageUrl){
+            imageView.kf.setImage(with: url, placeholder: UIActivityIndicatorView(style: .medium) as? Placeholder)
+        }
+        
+    }
+}
