e6debca831da4a4177c87c1deb06afc516564f72
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-19 20:55:03 +0300

    created: View for Starships

---
+++ StarWarsExplorer/Views/StarshipList/StarshipListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StarshipListViewController.swift
+//  StarWarsExplorer
+//
+//  Created by Bartuğ Kaan Çelebi on 17.01.2025.
+//
+
+import UIKit
+
+class StarshipListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
